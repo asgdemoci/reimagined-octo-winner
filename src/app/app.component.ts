c1cbd1c46257c238d0c757898d7ad19e86c70201
Borja Ruiz <borja.ruiz@asg.com>
2018-08-29 12:48:20 +0200

    add stupid changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    this.doSomethingA();
+    this.doSomethingB();
+  }
+
+  doSomethingA() {
+    for (let i=0; i<100; i++) {
+      if(i<40) {
+        if(i<20) {
+          if(i<10) {
+            if(i<40) {
+              console.log('Work');
+            }
+          }
+        }
+      }
+    }
+  }
+
+  doSomethingB() {
+    for (let i=0; i<100; i++) {
+      if(i<40) {
+        if(i<20) {
+          if(i<10) {
+            if(i<40) {
+              console.log('Work');
+            }
+          }
+        }
+      }
+    }
+  }
 }
